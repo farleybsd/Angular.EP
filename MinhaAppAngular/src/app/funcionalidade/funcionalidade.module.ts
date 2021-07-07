92a2721617ec27ac3604854a25f1b14b755ab8ad
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-07-07 14:16:38 -0300

    Modules e Decorators

---
+++ MinhaAppAngular/src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    // Import Componentes 
+  ],
+  imports: [
+    CommonModule
+    // Import Modulos
+  ],
+  providers:[
+   // Import Serviços
+  ]
+})
+export class FuncionalidadeModule { }
