f1b55527ad077453c4854bd845c893aab590e5fe
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-07-07 13:56:36 -0300

    Componentes

--- MinhaAppAngular/src/app/app.module.ts
+++ MinhaAppAngular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NomeDoComponenteComponent } from './pasta/nome-do-componente/nome-do-componente.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NomeDoComponenteComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
