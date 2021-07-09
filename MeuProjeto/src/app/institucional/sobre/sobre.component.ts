1df2cb63f5773e16ec51d359fea07a8a4b504421
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-07-09 10:59:36 -0300

    Roteamento e configuração

---
+++ MeuProjeto/src/app/institucional/sobre/sobre.component.ts
@@ -0,0 +1,16 @@
+import { Component,OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-sobre',
+  templateUrl: './sobreComponent.html',
+  styles: []
+})
+export class SobreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
